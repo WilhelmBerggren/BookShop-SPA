19026c46d00ea011d0ec756556c7a7baa8205a75
Wilhelm Berggren <wilhelmberggren@hotmail.com>
2020-02-16 21:59:31 +0100

    Fixed recursion on no key

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ async function displayBooks() {
     });
 }
 
-async function myFetch(params) {
-    params.key = await getKey();
+async function myFetch(params, useKey = true) {
+    if(useKey) params.key = await getKey();
     let url = new URL('https://www.forverkliga.se/JavaScript/api/crud.php');
     Object.entries(params).forEach(([key, value]) => url.searchParams.append(key, value));
     return await persistentFetch(url);
@@ -31,16 +31,19 @@ async function myFetch(params) {
 async function persistentFetch(url, n = 10) {
     document.querySelector('#attempts').innerHTML = 11 - n;
     if(n <= 0) return {};
-    let json = await fetch(url).then(res => res.json());
+    let json = await fetch(url)
+        .then(res => res.json())
+        .catch(e => {throw e});
     return (json.status == "success") ? json : persistentFetch(url, n-1);
 }
 
 async function getKey(refreshKey = false) {
     if(window.localStorage.getItem('key') == null || refreshKey == true) {
-        await myFetch({'requestKey': true}).then((k) => 
+        console.log("Getting new key...");
+        await myFetch({'requestKey': true}, false).then((k) => 
             window.localStorage.setItem('key', k.key)
         );
-    }    
+    }
     let key = window.localStorage.getItem('key');
     document.querySelector('#key').innerHTML = key;
     return key;
